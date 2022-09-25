328c448adebad54e8ec14cc396e6b288fa172071
DAN-MU-ZI <dlwogns6818@gmail.com>
2022-09-26 06:42:48 +0900

    solve import_meta_graph issue
    https://github.com/baiwenjia/ukbb_cardiac/issues/11 참조

--- utils/inference.py
+++ utils/inference.py
@@ -74,7 +74,6 @@ def render_sequence_meshes(audio_fname, sequence_vertices, template, out_path, u
         writer = cv2.VideoWriter(tmp_video_file.name, cv2.cv.CV_FOURCC(*'mp4v'), 60, (800, 800), True)
     else:
         writer = cv2.VideoWriter(tmp_video_file.name, cv2.VideoWriter_fourcc(*'mp4v'), 60, (800, 800), True)
-
     if os.path.exists(uv_template_fname) and os.path.exists(texture_img_fname):
         uv_template = Mesh(filename=uv_template_fname)
         vt, ft = uv_template.vt, uv_template.ft
@@ -92,11 +91,8 @@ def render_sequence_meshes(audio_fname, sequence_vertices, template, out_path, u
         img = render_mesh_helper(render_mesh, center, tex_img=tex_img)
         writer.write(img)
     writer.release()
-
     video_fname = os.path.join(out_path, 'video.mp4')
-    cmd = ('ffmpeg' + ' -i {0} -i {1} -vcodec h264 -ac 2 -channel_layout stereo -pix_fmt yuv420p {2}'.format(
-        audio_fname, tmp_video_file.name, video_fname)).split()
-    call(cmd)
+    os.system('ffmpeg -i {} -i {} -ac 2 {}'.format(audio_fname, tmp_video_file.name, video_fname))
 
 
 def inference(tf_model_fname, ds_fname, audio_fname, template_fname, condition_idx, out_path, render_sequence=True, uv_template_fname='', texture_img_fname=''):
@@ -109,8 +105,14 @@ def inference(tf_model_fname, ds_fname, audio_fname, template_fname, condition_i
 
     processed_audio = process_audio(ds_fname, audio, sample_rate)
 
+    gd = tf.MetaGraphDef()
+    with open(tf_model_fname + '.meta', "rb") as f:
+        gd.ParseFromString(f.read())
+    for node in gd.graph_def.node:
+        if '_output_shapes' in node.attr:
+            del node.attr['_output_shapes']
     # Load previously saved meta graph in the default graph
-    saver = tf.train.import_meta_graph(tf_model_fname + '.meta')
+    saver = tf.train.import_meta_graph(gd)
     graph = tf.get_default_graph()
 
     speech_features = graph.get_tensor_by_name(u'VOCA/Inputs_encoder/speech_features:0')
